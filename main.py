812b78e94b221347ee5596d832ca56bf8ff97d3a
Luis Barria <luis.barriar@uip.edu.pa>
2026-06-30 09:00:10 -0500

    Añadido programa base de la historia

---
+++ main.py
@@ -0,0 +1,11 @@
+# Solicitar datos al usuario
+
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+ciudad = input("Ingrese su ciudad de residencia: ")
+
+# Mostrar la información
+
+print("\n¡Hola,", nombre + "!")
+print("Tienes", edad, "años y vives en", ciudad + ".")
+print("Comienza aquí la historia de nuestro protagonista.")
