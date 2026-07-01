7069d42bf2f93d7d7df55369353703478768c212
Maria Barilla <maria.barilla@uip.edu.pa>
2026-07-01 15:15:16 -0500

    Agregar lineas narrativas a main.py
    Se agregó una continuación de la historia del protagonista para complementar la salida del programa

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ ciudad = input("Ingrese su ciudad de residencia: ")
 print("\n¡Hola,", nombre + "!")
 print("Tienes", edad, "años y vives en", ciudad + ".")
 print("Comienza aquí la historia de nuestro protagonista.")
+
+print(nombre, "salió de su casa con mucha curiosidad.")
+print("Mientras recorría las calles de", ciudad + ", encontró un antiguo libro.")
+print("Al abrirlo descubrió un mapa que lo llevaría a una aventura inolvidable.")
